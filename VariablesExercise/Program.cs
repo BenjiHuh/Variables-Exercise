2b19b03e96dab4cdcf2d3bf30ff4c63d3c682605
Benjamin Huh <benjaminayhuh@gmail.com>
2024-05-31 08:32:38 -0700

    attempted all tasks in variable declaration

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Benjamin";
+            int age = 18;
+            char favoriteChar = 's';
+            bool isFullstack = true;
+            double pi = 3.14;
+            decimal longerPi = 3.14159265358979323856264338m;
+            Console.WriteLine("Hello, World!" + $" \n My name is {name} and I am {age} years old.\n");
+            Console.WriteLine($"My favorite letter is {favoriteChar}");
+            Console.WriteLine($"It is {isFullstack} that I am currently bootcamping for the summer of 2024, to which I have " +
+                $"learned to print {pi} using string-interpolation and declare decimals like {longerPi} in addition to floats and doubles.");      
         }
     }
 }
